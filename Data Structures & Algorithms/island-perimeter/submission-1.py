78b176c1f58a206d06007f8c2a36204ddf39225e
Victoria Xiao <138067082+l3mxny@users.noreply.github.com>
2026-06-18 22:29:05 -0400

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+
+        #called upon on land 
+        def dfs(r,c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+                #it is a land
+                return 1
+            #neighboring water
+            if grid[r][c] == 0: 
+                return 1
+            #mark as visited
+            if grid[r][c] == 2:
+                return 0 
+            grid[r][c] = 2
+            return dfs(r-1,c) + dfs(r+1,c) + dfs(r,c+1) + dfs(r,c-1)           
+
+        #loop through grid
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                #start dfs on land
+                if grid[i][j] == 1:
+                    return dfs(i,j)
