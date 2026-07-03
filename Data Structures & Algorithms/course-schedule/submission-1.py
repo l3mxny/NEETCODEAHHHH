4bdcc70b83adcb7c234e3edde6bbda46fcd7ce27
Victoria Xiao <138067082+l3mxny@users.noreply.github.com>
2026-07-03 10:20:40 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visit = set()
+        path = set()
+        adj = {}
+        for i in range(numCourses):
+            adj[i] = []
+        for course,prereq in prerequisites:
+            adj[course].append(prereq)
+        def dfs(node):
+        #cycle
+            if node in path:
+                return False
+            #safe
+            if node in visit:
+                return True
+            path.add(node)
+            for neighbor in adj[node]:
+                #cycle in neighbor
+                if dfs(neighbor) == False:
+                    return False
+            #no cycle, add to safe visit
+            path.remove(node)
+            visit.add(node)
+            return True
+        for course in range(numCourses):
+            if dfs(course) is False:
+                return False
+        return True
+        
