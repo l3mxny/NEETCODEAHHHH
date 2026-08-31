19271c6f253f47c70a8d7524dfa85eac824db739
Victoria Xiao <138067082+l3mxny@users.noreply.github.com>
2026-08-30 20:35:59 -0400

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        num = n 
+        seen = set()
+        newNum = 0 
+
+        while num != 1:
+            seen.add(num)
+            while num > 0:
+                digit = num % 10 
+                newNum += digit * digit
+                num //= 10
+            nem = newNum
+            newNum = 0 
+            if num in seen:
+                return False
+        return True
+        
